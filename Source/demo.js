15954f98d6527f4648b05bd8c3721191fe227030
Boris Wetzel <boris.wetzel@iao.fraunhofer.de>
2012-03-19 12:42:24 +0100

    Updated demo

--- Source/demo.js
+++ Source/demo.js
@@ -21,5 +21,5 @@ window.addEvent('domready', function(){
 var tooltip1 = new AscTip($('tooltipDemo1'), 'This input field has no functionality');
 var tooltip2 = new AscTip($('tooltipDemo2'), 'The official Aegis Logo');
 var tooltip3 = new AscTip($('tooltipDemo3'), 'This button has no functionality');
-var tooltip4 = new AscTip($('tooltipDemo4'), 'This section contains dummy text');
+
 });
